10fb8c4239e50047e651a85c808395b0cb642a6a
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-25 21:28:33 +0530

    [Akash]Refactored: class method to compute Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,7 +5,7 @@ public class EmployeeWage{
 	public static final int NUM_OF_WORKING_DAYS=20; 
 	public static final int MAX_HRS_IN_MONTH=100;
 
-	public static void main(String[] args){
+	public static int computeEmpWage(){
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
 		//variables
@@ -34,5 +34,10 @@ public class EmployeeWage{
 		}
 		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: "+totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args){
+		computeEmpWage();
 	}
 }
